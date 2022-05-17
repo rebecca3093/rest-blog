0ad02cf1d0e8e5fc504555bf233b0e7ebfab8d74
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-05-17 13:43:51 -0500

    rest controllers backend upddates

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -1,10 +1,7 @@
 package com.example.restblog.web;
 
 import com.example.restblog.data.Post;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +15,9 @@ import java.util.Objects;
 
     @GetMapping
     public List<Post> getAll() {
-        posts.add(new Post(1L, "CoolTitle", "Cool Content"));
-        posts.add(new Post(2L, "CoolTitle 2", "Cool Content 2"));
-        posts.add(new Post(3L, "CoolTitle 3", "Cool Content 3"));
+        posts.add(new Post(1L, "This is my first title", "This is some content"));
+        posts.add(new Post(2L, "This is my second title", "This is more content"));
+        posts.add(new Post(3L, "This is my third title", "This is even more content"));
         return posts;
     }
 
@@ -34,6 +31,20 @@ import java.util.Objects;
         return new Post();
     }
 
+    @PostMapping
+        public void createPost(@RequestBody Post postItem){
+        System.out.println(postItem);
 
+    }
+
+    @PutMapping("{id}")
+    public void updatePost(@PathVariable Long id, @RequestBody Post updateItem){
+        System.out.println(updateItem);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePost(@PathVariable Long id){
+        System.out.println("Delete Post with id: " + id);
+    }
 }
 
